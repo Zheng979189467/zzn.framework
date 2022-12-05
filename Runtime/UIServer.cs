3b6e52677e68584bbdf968d53ecbf1a460a6e19f
Zheng979189467 <48668896+Zheng979189467@users.noreply.github.com>
2022-12-06 04:13:17 +0800

    .

--- Runtime/UIServer.cs
+++ Runtime/UIServer.cs
@@ -22,10 +22,36 @@ public class UIServer
    {
         builder.Register<UIServer>(Lifetime.Singleton);
         uIRoot =GameObject.Instantiate<UIRoot>( Resources.Load<UIRoot>("UIRoot"));
-        
+        foreach (var item in Assembly.GetExecutingAssembly().GetTypes())
+        {
+            if (item.IsSubclassOf(typeof(ViewModel)))
+            {
+                builder.Register(item, Lifetime.Singleton).As<ViewModel,IStartable>().AsSelf();
+            }
+        }
     }
-    public T OpenUIView<T>(UILevel uILevel=UILevel.Common) where T : Panel
+    public Dictionary<string, ViewPanel> panels;
+    public T GetUIView<T>() where T : ViewPanel
     {
+        if (panels == null)
+        {
+            panels = new Dictionary<string, ViewPanel>();
+            return null;
+        }
+        else if(panels.TryGetValue(typeof(T).Name, out var panel))
+        {
+            return panel as T;
+        }
+        return null;
+    }
+    public T OpenUIView<T>(UILevel uILevel=UILevel.Common) where T : ViewPanel
+    {
+        var panel = GetUIView<T>();
+        if (panel!=null)
+        {
+            panel.Show();
+            return panel;
+        }
         Transform tf;
         switch (uILevel)
         {
@@ -49,8 +75,21 @@ public class UIServer
                 break;
         }
        var res=GameObject.Instantiate<T>(ResourceLoadServer.Load<T>(typeof(T).Name),tf);
-       parent.Container.Inject(res); 
-       return res;
+        res.name = typeof(T).Name;
+       parent.Container.Inject(res);
+        if (panels == null)
+        {
+            panels = new Dictionary<string, ViewPanel>();
+        }
+        panels.Add(res.name, res);
+        return res;
+    }
+
+    internal void Close(ViewPanel viewPanel)
+    {
+        if (panels.ContainsValue(viewPanel))
+            panels.Remove(viewPanel.name);
+        UnityEngine.GameObject.Destroy(viewPanel.gameObject);
     }
 }
 public enum UILevel
@@ -80,22 +119,23 @@ public abstract class ViewModel : IViewModel,IStartable
     public abstract void Start();
     
 }
-public abstract class ViewPanel : Panel 
+public abstract class ViewPanel: Panel
 {
-   
+    [Inject]
+    public UIServer uIServer;
     public void Show()
     {
         gameObject.SetActive(true);
     }
     public void Discharge()
     {
-        Destroy(gameObject);
+        uIServer.Close(this);
     }
 }
 
 public abstract class Panel : MonoBehaviour, IPanel
 {
-  
+   
 }
 
 public interface IPanel
